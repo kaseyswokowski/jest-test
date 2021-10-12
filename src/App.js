5cb942731e4d7637f3fb52b6b5e47497a344026f
Kasey Swokowski <62677323+kaseyswokowski@users.noreply.github.com>
2021-10-12 19:48:58 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/todo';
 
 function App() {
+
+  const todo = [
+    {id: 1, title: 'wash dishes', completed: false,},
+    {id: 2, title: 'wash car', completed: true,},
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        todo.map((todo) => {
+          return (<Todo todo={todo}/>)
+        })
+      }
+      
     </div>
   );
 }
